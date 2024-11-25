2ca4e90fb4cf15ff0494b837905d9fa18e61c451
Binod <regbinod22@gmail.com>
2024-11-25 17:51:26 +0545

    Adding functionality

---
+++ components/countryDetailsShimmer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function countryDetailsShimmer() {
+  return (
+    <div className="country-details-container ">
+        <div className="back-button button-shimmer"></div>
+     <div className="country-details">
+        <div className="image"></div>
+        <div className='details-text'></div>
+    </div> 
+    </div>
+  )
+}
--- index.jsx
+++ index.jsx
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
       },
       {
         path: '/:country',
-        element: <CountryDetail />,
+        element:<CountryDetail />
       },
     ],
   },
