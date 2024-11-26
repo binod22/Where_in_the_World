a84dabd6f6eea668dd40080807d3fa838bee8e4b
Binod <regbinod22@gmail.com>
2024-11-26 13:02:45 +0545

    Implement Dark mode and Light mode

--- App.jsx
+++ App.jsx
@@ -1,12 +1,14 @@
 import Header from './components/Header'
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import { useState } from 'react'
 
 const App = () => {
+  const [isDark, setDark] = useState(JSON.parse(localStorage.getItem('isDarkMode')));
   return (
     <>
-      <Header />
-      <Outlet />
+      <Header theme={[isDark, setDark]} />
+      <Outlet context={[isDark, setDark]}/>
     </>
   )
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import {useState} from 'react'
 
-export default function Header() {
+
+export default function Header({theme}) {
+  const [isDark, setDark] = theme
   return (
     <div>
-      <header className="header-container">
+      <header className={`header-container ${isDark?'dark':''}`}>
       <div className="header-content">
         <h2 className="title">
           <a href="/">Where in the world?</a>
         </h2>
-        <p className="theme-changer">
-          <i className="fa-regular fa-moon" />
-          &nbsp;&nbsp;Dark Mode
+        <p className="theme-changer" onClick={()=>{
+          setDark(!isDark)
+          localStorage.setItem('isDarkMode',!isDark)
+        }}>
+          <i className ={`fa-regular fa-${isDark? 'sun':'moon'}`} />
+          &nbsp;&nbsp;{isDark ?'Light Mode' : 'Dark Mode'}
         </p>
       </div>
     </header>
